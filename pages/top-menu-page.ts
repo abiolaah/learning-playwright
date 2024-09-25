17d2c45e8a6c2cc2f93b5cea38a2bc33a2a7f917
victoriaajuwon <oluwapelumi.ajuwon@gmail.com>
2024-09-25 13:33:26 -0600

    Adding pages for POM

---
+++ pages/top-menu-page.ts
@@ -0,0 +1,61 @@
+import { expect, Locator, Page } from "@playwright/test";
+
+export class TopMenuPage {
+  //variables
+  readonly page: Page;
+  readonly getStartedLink: Locator;
+  readonly docLink: Locator;
+  readonly apiLink: Locator;
+  readonly nodeLink: Locator;
+  readonly pythonLink: Locator;
+  readonly javaLink: Locator;
+  readonly dotNetLink: Locator;
+  readonly communityLink: Locator;
+  readonly nodeLabel: Locator;
+  readonly javaLabel: Locator;
+  readonly nodeDescription: "Installing Playwright";
+  readonly javaDescription: `Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation.`;
+
+  //constructor
+  constructor(page: Page) {
+    this.page = page;
+    this.getStartedLink = page.getByRole("link", { name: "Get Started" });
+    this.docLink = page.getByRole("link", { name: "Docs" });
+    this.apiLink = page.getByRole("link", { name: "API" });
+    this.nodeLink = page.getByRole("button", { name: "Node.js" });
+    this.pythonLink = page
+      .getByRole("navigation", { name: "Main" })
+      .getByText("Python");
+    this.javaLink = page
+      .getByRole("navigation", { name: "Main" })
+      .getByText("Java");
+    this.dotNetLink = page
+      .getByRole("navigation", { name: "Main" })
+      .getByText(".NET");
+    this.communityLink = page.getByRole("link", { name: "Community" });
+    this.nodeLabel = page.getByText(this.nodeDescription, { exact: true });
+    this.javaLabel = page.getByText(this.javaDescription);
+  }
+  //methods
+  async hoverNode() {
+    await this.nodeLink.hover();
+  }
+
+  async clickJava() {
+    await this.javaLink.click();
+  }
+
+  async assertPageUrl(pageUrl: RegExp) {
+    await expect(this.page).toHaveURL(pageUrl);
+  }
+
+  async assertNodeDescriptionNotVisible() {
+    await expect(this.nodeLabel).not.toBeVisible();
+  }
+
+  async assertJavaDescriptionVisible() {
+    await expect(this.javaLabel).toBeVisible();
+  }
+}
+
+export default TopMenuPage;
